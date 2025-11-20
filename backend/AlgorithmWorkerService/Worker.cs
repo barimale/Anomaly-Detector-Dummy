750cfb698d3691c888bddff32dac59aa28f3baab
Mateusz <mateusz.wolnica@gmail.com>
2025-11-20 20:21:00 +0100

    .

--- backend/AlgorithmWorkerService/Worker.cs
+++ backend/AlgorithmWorkerService/Worker.cs
@@ -2,6 +2,7 @@ using Common.RabbitMQ;
 using Common.RabbitMQ.Model;
 using MSSql.Infrastructure.Entities;
 using MSSql.Infrastructure.Repositories.Abstractions;
+using Questdb.Net;
 using RabbitMQ.Client.Events;
 using System.Text.Json;
 
@@ -9,12 +10,16 @@ namespace Algorithm.A.WorkerService {
     public class Worker : BackgroundService {
         private readonly ILogger<Worker> _logger;
         private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IQuestDBClient questDbClient;
+
         //private readonly IML executorService;
 
         public Worker(ILogger<Worker> logger,
-            IServiceScopeFactory _scopeFactory) {
+            IServiceScopeFactory _scopeFactory,
+            IQuestDBClient questDbClient) {
             _logger = logger;
             this._scopeFactory = _scopeFactory;
+            this.questDbClient = questDbClient;
             //this.executorService = executorService;
         }
 
